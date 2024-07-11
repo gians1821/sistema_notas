0e01aa4a797e490970683d3aba7b27c93052bf69
gians1821 <gian.samana.1801@gmail.com>
2024-07-11 15:27:27 -0500

    añadiendo los modelos del proyecto anterior

---
+++ app/Models/Producto.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Producto extends Model
+{
+    use HasFactory;
+
+    protected $table = 'productos';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'descripcion', 'categoria_id', 'unidad_id', 'stock', 'estado'
+    ];
+
+    public function categoria()
+    {
+        return $this->hasOne(Categoria::class, 'idcategoria', 'categoria_id');
+    }
+
+    public function unidad()
+    {
+        return $this->hasOne(Unidad::class, 'id', 'unidad_id');
+    }
+}
