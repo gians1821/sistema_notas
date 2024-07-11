56c50a1a104ca46deaa0d597e348082f7cd8501e
gians1821 <gian.samana.1801@gmail.com>
2024-07-11 16:23:38 -0500

    añadiendo tabla de categorias

---
+++ database/migrations/2024_07_11_211007_create_categorias_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categorias', function (Blueprint $table) {
+            $table->id('idcategoria'); // Definimos la clave primaria
+            $table->string('descripcion');
+            $table->boolean('estado');
+            // No se incluyen timestamps ya que en el modelo se indicó $timestamps = false;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categorias');
+    }
+}
